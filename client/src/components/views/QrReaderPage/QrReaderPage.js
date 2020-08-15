d16385423be2dfdc531d8237006109102b9c03cf
Kichan Yoon <pro4697@naver.com>
2020-08-15 23:55:47 +0900

    QrReader 추가

--- client/src/components/views/QrReaderPage/QrReaderPage.js
+++ client/src/components/views/QrReaderPage/QrReaderPage.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Cameras, Scanner } from 'react-instascan';
 
 function QrReaderPage() {
-  return <div style={{ width: '100%', height: '100%' }}>QrReaderPage</div>;
+  const onScan = (e) => {
+    console.log(e.currentTarget);
+  };
+
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <Cameras>
+        {(cameras) => (
+          <div>
+            <h1>Scan the code!</h1>
+            <Scanner camera={cameras[0]} onScan={onScan}>
+              <video style={{ width: 400, height: 400 }} />
+            </Scanner>
+          </div>
+        )}
+      </Cameras>
+    </div>
+  );
 }
 
 export default QrReaderPage;
