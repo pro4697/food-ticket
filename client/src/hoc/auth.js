6ce157ea28401674c473ffce323392c6f45a8c11
Kichan Yoon <pro4697@naver.com>
2021-01-07 16:09:58 +0900

    버그 수정
    비로그인 상태에서 식권 보관함 진입시 로그인 화면으로 간 후, 뒤로가기시 루프에 걸리는점 수정

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -2,30 +2,32 @@ import React, { useEffect } from 'react';
 import { auth } from '../_actions/user_actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { headersConfig } from '../components/Config';
+
 export default function (ComposedClass, reload, adminRoute = null) {
-  function AuthenticationCheck(props) {
-    let user = useSelector((state) => state.user);
-    const dispatch = useDispatch();
+	function AuthenticationCheck(props) {
+		let user = useSelector((state) => state.user);
+		const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(auth(headersConfig)).then(async (response) => {
-        if (await !response.payload.isAuth) {
-          if (reload) {
-            props.history.push('/login');
-          }
-        } else {
-          if (adminRoute && !response.payload.isAdmin) {
-            props.history.push('/login');
-          } else {
-            if (reload === false) {
-              props.history.push('/');
-            }
-          }
-        }
-      });
-    }, [dispatch, props.history]);
+		useEffect(() => {
+			dispatch(auth(headersConfig)).then(async (response) => {
+				if (await !response.payload.isAuth) {
+					if (reload) {
+						props.history.push('/');
+						props.history.push('/login');
+					}
+				} else {
+					if (adminRoute && !response.payload.isAdmin) {
+						props.history.push('/login');
+					} else {
+						if (reload === false) {
+							props.history.push('/');
+						}
+					}
+				}
+			});
+		}, [dispatch, props.history]);
 
-    return <ComposedClass {...props} user={user} />;
-  }
-  return AuthenticationCheck;
+		return <ComposedClass {...props} user={user} />;
+	}
+	return AuthenticationCheck;
 }
