aabe9a9c6a9ad1f613c2c49f4e01e2f8e4aabfcb
Kichan Yoon <pro4697@naver.com>
2021-04-15 23:43:53 +0900

    chore(social-login): fallback link 수정

--- src/components/SocialLogin/Github.tsx
+++ src/components/SocialLogin/Github.tsx
@@ -49,7 +49,7 @@ const Span = styled.div`
   width: 80%;
 `;
 
-const fallback = 'https://pro4697.github.io/food-ticket/githubLogin';
+const fallback = 'https://foodticket.xyz/githubLogin';
 
 function GithubBtn({ callback = true }) {
   const link = `https://github.com/login/oauth/authorize?client_id=b1ca53d55037a1da7d82&redirect_uri=${fallback}`;
